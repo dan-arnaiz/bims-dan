8181c52e05097955a106760f7311acd993e475fc
Restyled.io <commits@restyled.io>
2024-06-23 11:47:02 +0000

    Restyled by astyle

--- Form1.cs
+++ Form1.cs
@@ -10,21 +10,21 @@ using System.Windows.Forms;
 
 namespace BIMS_dan
 {
-    public partial class Form1 : Form
+public partial class Form1 : Form
+{
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        InitializeComponent();
     }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
 }
