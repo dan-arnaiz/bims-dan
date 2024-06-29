9cc74010cf4d6996d5d837e9a52344f53793c5f6
dan-arnaiz <dfarnaiz@gmail.com>
2024-06-30 00:21:02 +0800

    Before database

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace BIMS_dan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainDashboard());
+            Application.Run(new SignInForm());
         }
     }
 }
