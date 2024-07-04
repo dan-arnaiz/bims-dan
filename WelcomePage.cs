7cffafc59348eb02a61f43d9380739be0176229d
Dan Arnaiz <dfarnaiz@gmail.com>
2024-07-04 08:05:02 +0800

    AuthenticationDONE

---
+++ WelcomePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIMS_dan
+{
+    public partial class WelcomePage : Form
+    {
+        // Timer declaration
+        private Timer timer = new Timer();
+
+        public WelcomePage()
+        {
+            InitializeComponent();
+            // Timer setup
+            timer.Interval = 1000; // Set timer for 1 second
+            timer.Tick += Timer_Tick; // Event handler for Timer Tick
+        }
+
+        private void WelcomePage_Load(object sender, EventArgs e)
+        {
+            timer.Start(); // Start the timer when the form loads
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop(); // Stop the timer to prevent it from ticking again
+
+            // Close the current WelcomePage form
+            this.Close();
+
+            // Open the SignInPage form
+            SignInForm signInPage = new SignInForm();
+            signInPage.Show();
+        }
+    }
+}
